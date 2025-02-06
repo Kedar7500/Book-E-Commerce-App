d8ee71b13aecde85f384b780554860cef6daf4cb
72591_Kedar <kedar.jagtap999@gmail.com>
2025-02-06 16:15:33 +0530

    project structure and category model

---
+++ Book E-Commerce/Book E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Book_E_Commerce.Data;
+using Book_E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = dbContext.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
